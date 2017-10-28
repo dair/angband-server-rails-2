a068f716306bc765e939b67988f531c80cbe678c
Vladimir Lebedev-Schmidthof <dair@albiongames.org>
2017-10-28 17:00:37 +0300

    Empty login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    def login
+    def store_return_to
+        session[:return_to] = request.referrer
+        puts 'STORING: ' + session[:return_to]
     end
 
+    def login
+        store_return_to
+        flash[:login] = true
+    end
+
+    def do_login
+        user = params[:login]
+        password_hash = params[:hash]
+
+
+        puts session[:return_to]
+
+        # success
+        redirect_to(session[:return_to])
+        session[:return_to] = nil
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     root to: 'reader#games'
-    
-    post "application/login"
+
+    get "login" => "application#login"
+    post "do_login" => "application#do_login"
 
     get "events_j" => "reader#events_j"
     get "objects_j" => "reader#objects_j"
